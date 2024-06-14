5c94295c49b2eced913cf067b522af886e52410f
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-06-14 01:07:48 -0700

    feat(backend-authRoute): add logic to hash password,generate jwt-token and set cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTOkenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -19,24 +21,33 @@ export const signup = async (req, res) => {
     }
 
     // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPass,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
-    await newUser.save();
-    res.status(200).json({
-      success: true,
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+
+    if (newUser) {
+      // generate jwt token
+      generateTOkenAndSetCookie(newUser._id, res);
+      await newUser.save();
+      res.status(200).json({
+        success: true,
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ success: false, error: "Invalid user data!" });
+    }
   } catch (error) {
     console.log("Error in signup controller", error);
     res.json({
